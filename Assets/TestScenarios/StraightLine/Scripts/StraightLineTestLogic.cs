3d87497f923878f4019296c7ea9f9a80fa92cadc
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2017-05-28 18:47:06 +0300

    Tweaks to test scenario

--- Assets/TestScenarios/StraightLine/Scripts/StraightLineTestLogic.cs
+++ Assets/TestScenarios/StraightLine/Scripts/StraightLineTestLogic.cs
@@ -10,20 +10,13 @@ namespace Assets.TestScenarios.StraightLine.Scripts
             if (Time.time < 4.0f)
                 return 200.0f;
 
-            /*if (Time.time < 14.0f)
-                return 3.0f*200.0f;
-            else
-            {
-                return 0;
-            }*/
-
             if (slip < slipLimit)
-                return currentTorque + 1.0f;
+                return currentTorque + 2.0f;
 
             if (currentTorque > 1.0f && slip > slipLimit)
             {
                 tractionControl = true;
-                return currentTorque - 1.0f;
+                return currentTorque - 2.0f;
             }
             
             return currentTorque;

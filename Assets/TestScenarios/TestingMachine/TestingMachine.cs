914393197f7cc843b7eebbc8a2d7533807b212d3
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2017-07-02 19:51:06 +0300

    Added tyre testing machine

---
+++ Assets/TestScenarios/TestingMachine/TestingMachine.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Plugins.PrimeTyre;
+using UnityEngine;
+
+public class TestingMachine : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody Rigid;
+
+    [SerializeField]
+    private PrimeTyre Tyre; 
+
+    private float _maxSteeringAngle = 30.0f;
+    private float _maxBrakeTorque = 6000.0f;
+    private float _speed = 4.0f;
+
+    void Update () {
+        Rigid.velocity = new Vector3(0, 0, _speed);
+
+        Tyre.BrakeTorque = 0.0f;
+        if (Input.GetButton("Jump"))
+        {
+            Tyre.BrakeTorque = _maxBrakeTorque;
+        }
+
+        var joystickValue = Input.GetAxis("Horizontal");
+        Tyre.SteeringAngle = _maxSteeringAngle * joystickValue;
+    }
+}

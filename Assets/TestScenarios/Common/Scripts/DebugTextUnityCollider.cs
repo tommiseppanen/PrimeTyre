d16a00bd45ddf074a26bc1219a4f8266eb85a65f
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2017-08-13 11:54:12 +0300

    Added testing machine for Unity's wheel collider

---
+++ Assets/TestScenarios/Common/Scripts/DebugTextUnityCollider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Plugins.PrimeTyre;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.TestScenarios.Cornering.Scripts
+{
+    public class DebugTextUnityCollider : MonoBehaviour
+    {
+        [SerializeField]
+        private List<WheelCollider> _tyres;
+
+        [SerializeField]
+        private Rigidbody _rigidbody;
+
+        private Text _text;
+        private float _previousVelocity = 0.0f;
+
+        void Start ()
+        {
+            _text = GetComponent<Text>();
+        }
+	
+        void Update ()
+        {
+            var info = _tyres.Select(t =>
+            {
+                WheelHit hit;
+                t.GetGroundHit(out hit);
+                return string.Format("{0,-10:F2} {1,-10:F2} {2,-10:F0}",
+                    hit.forwardSlip, hit.sidewaysSlip, hit.force);
+            });
+            _text.text = info.Aggregate(string.Empty, (combined, next) => combined + "\n" + next);
+            var velocity = _rigidbody.velocity.magnitude;
+            _text.text += string.Format("\n{0:F2} {1:F2}", velocity, (velocity - _previousVelocity) / Time.deltaTime);
+            _previousVelocity = velocity;
+        }
+    }
+}
---
+++ Assets/TestScenarios/TestingMachine/TestingMachineUnityCollider.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class TestingMachineUnityCollider : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody Rigid;
+
+    [SerializeField]
+    private WheelCollider Tyre; 
+
+    private float _maxSteeringAngle = 30.0f;
+    private float _maxBrakeTorque = 2000.0f;
+    private float _maxMotorTorque = 1000.0f;
+    private float _speed = 4.0f;
+
+    void Update () {
+        Tyre.brakeTorque = 0.0f;
+
+        var horizontalValue = Input.GetAxis("Horizontal");
+        Tyre.steerAngle = _maxSteeringAngle * horizontalValue;
+
+        var verticalValue = Input.GetAxis("Vertical");
+        if (verticalValue < 0.0f)
+            Tyre.brakeTorque = -verticalValue * _maxBrakeTorque;
+        else
+            Tyre.motorTorque = verticalValue * _maxMotorTorque;
+
+        if (Input.GetButton("Jump"))
+            Tyre.brakeTorque = _maxBrakeTorque;
+    }
+
+    private void FixedUpdate()
+    {
+        Rigid.velocity = new Vector3(0, 0, _speed);
+    }
+}
